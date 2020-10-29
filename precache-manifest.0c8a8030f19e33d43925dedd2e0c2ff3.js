837dacf87c224550d829fc706ec6ceb79494e449
Chris Lovett <clovett@microsoft.com>
2020-10-29 12:49:31 -0700

    initial commit

---
+++ precache-manifest.0c8a8030f19e33d43925dedd2e0c2ff3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14d07ccae32e78ca2baf5f80b6387076",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2830376d2429e080e95",
+    "url": "/static/css/main.daf23beb.chunk.css"
+  },
+  {
+    "revision": "299a587c78cba909d8dd",
+    "url": "/static/js/2.3d5e6969.chunk.js"
+  },
+  {
+    "revision": "6e742f513d9938d07b2d93218f3e4449",
+    "url": "/static/js/2.3d5e6969.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2830376d2429e080e95",
+    "url": "/static/js/main.c2079f58.chunk.js"
+  },
+  {
+    "revision": "c2dae2c1d784a7fc8198",
+    "url": "/static/js/runtime-main.ac33299d.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
